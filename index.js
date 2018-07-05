581fcb7620605f6ffb42e7b5c3209339814a4893
Tristan Brown <tgbrown@stanford.edu>
2018-07-05 00:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,18 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
  }	 
 
-function currentLine (katzDeli) {
-  var i = 0 
+var line = [];
++function currentLine(katzDeli) {
+  let i = 0;
   while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
     i++;
-  }
-  if (katzDeli.length > 0) {
-    return 
-  }
++  }
++  if (katzDeli.length === 0) {
++    return "The line is currently empty.";
++  } else
++  return(`The line is currently:` + line);
++}
 }
 
 
